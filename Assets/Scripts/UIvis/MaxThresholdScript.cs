c5380680aaa4ced24ef443de20e1e0bc988f0b93
Odgaard <rasmus.odgaard18@gmail.com>
2024-04-30 10:28:11 +0200

    Refillable Healthbar Added

--- Assets/Scripts/UIvis/MaxThresholdScript.cs
+++ Assets/Scripts/UIvis/MaxThresholdScript.cs
@@ -8,21 +8,28 @@ public class MaxThresholdScript : MonoBehaviour
 {
     public static MaxThresholdScript Instance;
     
-    [HideInInspector]
+    //BCI Meter
     public Slider MaxSlider;
     public Slider absoluteSlider;
     
-    public float maxHealth = 100;
-
-    public float currentHealth = 100;
+    //GameObject
+    public GameObject GameObject;
+    public Image healthBarImage;
+    public Image HealthBarImageRed;
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
     private float _damageAmount;
-    
+    public float fillSpeed = 1f;
+
+    //Coroutine
     private Coroutine _myCoroutine;
     private bool _isCoroutineRunning = false;
 
-   /* private void Awake()
+    
+    
+    private void Awake()
     {
-        if (Instance != null)
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -32,11 +39,10 @@ public class MaxThresholdScript : MonoBehaviour
         }
         DontDestroyOnLoad(this);
     }
-    */
 
     private void Start()
     {
-        MaxSlider = GetComponent<Slider>();
+        //MaxSlider = GetComponent<Slider>();
         
     }
 
@@ -80,13 +86,49 @@ public class MaxThresholdScript : MonoBehaviour
     public void TakeDamage()
     {
         currentHealth -= _damageAmount;
+        UpdateHealthBar();
         Debug.Log("Health:"+currentHealth);
         
         if (currentHealth <= 0)
         {
             currentHealth = maxHealth;
+            
             Debug.Log("Try: Destroy Wood");
         }
+        
+    }
+    
+    void UpdateHealthBar()
+    {
+        float fillAmount = currentHealth / maxHealth;
+        HealthBarImageRed.fillAmount = fillAmount;
+        StartCoroutine(LerpFillAmount(fillAmount));
+    }
+    
+    IEnumerator LerpFillAmount(float targetFillAmount)
+    {
+        float originalFillAmount = healthBarImage.fillAmount;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fillSpeed)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = 1f - Mathf.Pow(1f - (elapsedTime / fillSpeed), 3); // Exponential interpolation
+            healthBarImage.fillAmount = Mathf.Lerp(originalFillAmount, targetFillAmount, t);
+            yield return null;
+        }
+
+        healthBarImage.fillAmount = targetFillAmount;
+        if (healthBarImage.fillAmount == 0)
+        {
+            Debug.Log("YoWhat");
+            //yield return new WaitForSeconds(2f);
+            HealthBarImageRed.fillAmount = 1;
+            healthBarImage.fillAmount = 1;
+            Debug.Log("The fuck");
+        }
+        
+        
         
     }
     
